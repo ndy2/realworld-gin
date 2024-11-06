cbb7c763b622f86fba9e121e2d9bb6120e67b487
ndy2 <emrdbs12@gmail.com>
2024-11-07 00:06:54 +0900

    update init-db.sql

--- resources/database/init-db.sql
+++ resources/database/init-db.sql
@@ -3,12 +3,12 @@ CREATE DATABASE IF NOT EXISTS realworld;
 USE realworld;
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    id         INT          AUTO_INCREMENT PRIMARY KEY,
+    username   VARCHAR(255) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL,
+    updated_at TIMESTAMP    NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS profiles (
@@ -18,4 +18,12 @@ CREATE TABLE IF NOT EXISTS profiles (
     image      TEXT,
     created_at TIMESTAMP    NOT NULL,
     updated_at TIMESTAMP    NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS followers (
+    id          INT          AUTO_INCREMENT PRIMARY KEY,
+    user_id     INT          NOT NULL,
+    follower_id INT          NOT NULL,
+    created_at  TIMESTAMP    NOT NULL,
+    updated_at  TIMESTAMP    NOT NULL
 );
