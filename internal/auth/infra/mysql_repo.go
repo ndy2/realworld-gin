29b48366a6918a8f22943c8bfe72240f9b56eb13
ndy2 <emrdbs12@gmail.com>
2024-11-09 22:12:51 +0900

    clear import

--- internal/auth/infra/mysql_repo.go
+++ internal/auth/infra/mysql_repo.go
@@ -5,8 +5,8 @@ import (
 	"errors"
 	"fmt"
 	"go.uber.org/zap"
-	auth3 "ndy/realworld-gin/internal/auth/app"
-	auth2 "ndy/realworld-gin/internal/auth/domain"
+	"ndy/realworld-gin/internal/auth/app"
+	"ndy/realworld-gin/internal/auth/domain"
 	"ndy/realworld-gin/internal/util"
 )
 
@@ -20,31 +20,31 @@ func NewMysqlRepo(db *sql.DB) *MysqlRepo {
 }
 
 // FindUserByEmail 는 주어진 사용자가 있는지 확인합니다.
-func (repo *MysqlRepo) FindUserByEmail(email string) (auth2.User, error) {
-	var user auth2.User
+func (repo *MysqlRepo) FindUserByEmail(email string) (domain.User, error) {
+	var user domain.User
 	query := "SELECT id, username, email, password FROM users WHERE email = ?"
 	err := repo.db.QueryRow(query, email).Scan(&user.Id, &user.Username, &user.Email, &user.Password)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return auth2.User{}, fmt.Errorf("%w: %v", auth3.ErrUserNotFound, err)
+			return domain.User{}, fmt.Errorf("%w: %v", app.ErrUserNotFound, err)
 		}
 		util.Log.Error("FindUserByEmail failed", zap.Error(err))
-		return auth2.User{}, err
+		return domain.User{}, err
 	}
 	return user, nil
 }
 
 // FindProfileByUserID 는 주어진 사용자 ID에 해당하는 프로필을 반환합니다.
-func (repo *MysqlRepo) FindProfileByUserID(userID int) (auth2.Profile, error) {
-	var profile auth2.Profile
+func (repo *MysqlRepo) FindProfileByUserID(userID int) (domain.Profile, error) {
+	var profile domain.Profile
 	query := "SELECT id, user_id, bio, image FROM profiles WHERE user_id = ?"
 	err := repo.db.QueryRow(query, userID).Scan(&profile.Id, &profile.UserID, &profile.Bio, &profile.Image)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			return auth2.Profile{}, fmt.Errorf("%w: %v", auth3.ErrProfileNotFound, err)
+			return domain.Profile{}, fmt.Errorf("%w: %v", app.ErrProfileNotFound, err)
 		}
 		util.Log.Error("FindProfileByUserID failed", zap.Error(err))
-		return auth2.Profile{}, err
+		return domain.Profile{}, err
 	}
 	return profile, nil
 }
